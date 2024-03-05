cf75c621b9aa72ae86eadbeaa4112de5acebc6f6
VladyslavFiniahin <finyagin07@gmail.com>
2024-03-05 23:28:20 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import Home from './pages/Home/home';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Error from './pages/404/404'
+import Accaunt from './pages/Account/accaunt';
+
+/*
+import React, {useState, useEffect} from 'react'
+
+function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('/api')
+    .then(response => response.json())
+    .then(response => setData(response.message))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {
+            !data ? 'Loading...' : data
+          }
+        </p>
+
+      </header>
+    </div>
+  );
+}
+*/
+
+class App extends React.Component {
+  
+  render() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" Component={Home}/>
+          <Route path="*" Component={Error}/>
+          <Route path='/accaunt-settings' Component={Accaunt}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ frontend/src/pages/Home/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './home.css';
+
+export class Home extends Component {
+  render() {
+    return (
+      <body>
+
+            <header>
+              <a href="/accaunt-settings" className='text'><img src='./img/UserPhotoS.png' alt='' className='profile-settings'/></a>
+            </header>
+   
+
+
+      </body>
+    )
+  }
+}
+
+export default Home
