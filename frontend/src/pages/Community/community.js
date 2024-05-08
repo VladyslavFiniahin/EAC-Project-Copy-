7049e9532d6aa8aa0cfd7be04a5c1a99592ffa57
VladyslavFiniahin <finyagin07@gmail.com>
2024-05-08 21:47:02 +0300

    community

--- frontend/src/pages/Community/community.js
+++ frontend/src/pages/Community/community.js
@@ -1,66 +1,85 @@
-import React, { Component } from 'react'
-import './community.css'
+import React, { Component } from 'react';
+import './community.css';
 
-export class Community extends Component {
+class Community extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      userData: null,
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { username } = this.state;
+    try {
+      const response = await fetch(`/api/users?username=${username}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const userData = await response.json();
+      this.setState({ userData });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   render() {
+  const { userData } = this.state;
     return (
       <>
-      <body>
         <header>
-        <a href="/accaunt-settings" className='text'><img src='./img/UserPhotoS.png' alt='' className='profile-settings'/></a>
-        <a href="/settings" className='text'><img src='./img/Menu.png' alt='' className='settings'/></a>
-      </header>
+          <a href="/accaunt-settings" className='text'><img src='./img/UserPhotoS.png' alt='' className='profile-settings'/></a>
+          <a href="/settings" className='text'><img src='./img/Menu.png' alt='' className='settings'/></a>
+        </header>
 
-      <div className='bg-commu'>
-        <div className='cont-commu'>
-            <div className='fstr1'>Make new <br/>friends</div>
-            <div className='fstr2'>By adding a new friends to your list, you 
-            <br/>can view their activity during the day</div>
-        </div>
-      </div>
-
-      <div className='bg-search'>
-        <div className='cont-search'>
-            <input className='inp-search' type="text"  placeholder="Enter username"/>
-            <img src='./img/search.png' alt='' className='search12'/>
-            <div className='box-of-icon'></div>
-        </div>
-      </div>
-
-      <div className='bg-friends'>
-        <div className='cont-friends'>
-          <div className='list'>Your list of friends:</div>
-          <p>Friend 1</p>
-          <div className='bgoflist'>
-            <img src='./img/User35x35.png' alt='' className='usr1'/>
-            <div className='contoflist'><div className='tupo'>(Username) average emissions <br/> are reduced by <span>36%</span></div> </div>
-          </div>
-          <p>Friend 2</p>
-          <div className='bgoflist'>
-            <img src='./img/User35x35.png' alt='' className='usr1'/>
-            <div className='contoflist'><div className='tupo'>(Username) average emissions <br/> are reduced by <span>36%</span></div> </div>
-          </div>
-          <p>Friend 3</p>
-          <div className='bgoflist'>
-            <img src='./img/User35x35.png' alt='' className='usr1'/>
-            <div className='contoflist'><div className='tupo'>(Username) have not been <br/> active this day</div> </div>
+        <div className='bg-commu'>
+          <div className='cont-commu'>
+              <div className='fstr1'>Make new <br/>friends</div>
+              <div className='fstr2'>By adding a new friends to your list, you 
+              <br/>can view their activity during the day</div>
           </div>
         </div>
-      </div>
 
-      
-      <footer>
-        <div className='footer77'>
-            <a href="/home" className='text'><img src='./img/Home.png' alt=''/></a>
-            <a href="/share" className='text'><img src='./img/Share.png' alt=''/></a>
-            <a href="/community" className='text'><img src='./img/Community.png' alt=''/></a>
-            <a href="/activity" className='text'><img src='./img/Activity.png' alt=''/></a>
+        <div className='bg-search'>
+          <div className='cont-search'>
+              <form onSubmit={this.handleSubmit}>
+                <input className='inp-search' type="text" placeholder="Enter username" onChange={this.handleChange} />
+                <button type="submit" className='btac'><img src='./img/search.png' alt='' className='search12'/></button>
+              </form>
+          </div>
         </div>
-      </footer>
-      </body>
+
+        <div className='bg-friends'>
+          <div className='cont-friends'>
+            <div className='list'>Your list of friends:</div>
+            {userData && userData.map(user => (
+              <div key={user.username}>
+                <div className='bgoflist'>
+                  <img src='./img/User35x35.png' alt='' className='usr1'/>
+                  <div className='contoflist'><div className='tupo'>{user.username} average emissions <br/> are reduced by <span>36%</span></div> </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <footer>
+          <div className='footer77'>
+              <a href="/home" className='text'><img src='./img/Home.png' alt=''/></a>
+              <a href="/share" className='text'><img src='./img/Share.png' alt=''/></a>
+              <a href="/community" className='text'><img src='./img/Community.png' alt=''/></a>
+              <a href="/activity" className='text'><img src='./img/Activity.png' alt=''/></a>
+          </div>
+        </footer>
       </>
-    )
-  }
+    );
+ }
 }
 
-export default Community
+export default Community;
